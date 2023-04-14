54970e31b7cd23a2aa3120355c71a3afcc9c80f2
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-04-14 14:04:17 +0500

    feat:add find and root and add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,27 +8,74 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.rootNode = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootNode;
   }
+
+  add( data ) {
+    let newNode = new Node(data);
+    
+    if(this.rootNode === null){
+      this.rootNode = newNode;
+      return;
+    }
+    
+    function recursing(node){
+      let c;
+      
+      if(node.data>newNode.data){
+        c = node.left;
+      }else {
+        c = node.right;
+      }
+     
+      if(c === null) {
+        return node;
+      } else {
+        return recursing(c);
+      }
+
+    }
+    
+    let targetNode = null; 
+    targetNode = recursing (this.rootNode);
+   
+    if(newNode.data < targetNode.data){
+      targetNode.left = newNode;
+    } else {
+      targetNode.right = newNode;
+    }
+
+  
+}
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function recursing(node){
+      if (node === null) {
+        return null;
+      }
+  
+      if (node.data === data) {
+        return node;
+      } else if(node.data > data) {
+        return recursing(node.left);
+      } else{
+        return recursing(node.right);
+      }
+    }
+     return recursing(this.rootNode);
   }
 
-  remove(/* data */) {
+  remove(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
